742d2c68fc66975e938e3f85d2570f74a0497712
ShawnReardon <shawnreardon1@gmail.com>
2020-10-22 20:38:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+class conuctionJunction{
+
+and(text1: string, text2: string) {
+    return "" + text1 + " and " + text2
+}
+
+or(text1: string, text2: string) {
+    return "" + text1 + " or " + text2
+}
+
+
+}
+let tester = new conuctionJunction();
+game.showLongText(tester.and("shawn", "good"), DialogLayout.Bottom)
+game.showLongText(tester.or("shawn", "good"), DialogLayout.Bottom)
